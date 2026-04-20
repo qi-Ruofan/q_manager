345d0684aef448e2dbdf50240caadcd79405248b
qrf <1521656315@qq.com>
2026-04-20 19:18:10 +0800

    layout init

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
-    <h1>QManager</h1>
-    <router-view></router-view>
-  </div>
+  <a-layout :style="layoutStyle">
+    <a-layout-sider :style="siderStyle">Sider</a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout-content :style="contentStyle">
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { CSSProperties } from "vue";
+const layoutStyle: CSSProperties = {
+  height: "100vh",
+};
+const headerStyle: CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
+};
+
+const contentStyle: CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9",
+};
+
+const siderStyle: CSSProperties = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9",
+};
+</script>
 
 <style lang="scss" scoped></style>
